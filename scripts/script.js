811529f0e411d85be12956165f9250c168d0a2a6
N1ck <nicholas.o.lockhart@gmail.com>
2016-10-31 12:29:42 +1300

    Add context menu

--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,7 @@ const lgtmButton = $('<button />',
     id: 'lgtm-button',
     click: (e) => {
       e.preventDefault()
-      const authenticityToken = $('input[name="authenticity_token"]').val()
-      const headSha = $('input[name="head_sha"]').val()
-
-      $.ajax({
-        type: 'POST',
-        url: `${window.location.pathname}/reviews`,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        },
-        data: {
-          '_method': 'put',
-          'authenticity_token': authenticityToken,
-          'head_sha': headSha,
-          'pull_request_review[event]': 'approve',
-          'pull_request_review[body]': 'LGTM'
-        }
-      })
+      submitLGTM()
     }
 })
 
@@ -30,6 +14,38 @@ const init = () => {
     $('#partial-new-comment-form-actions').append(lgtmButton)
   }
 }
+
+const submitLGTM = () => {
+  let formButtons = $('.form-actions button')
+  const authenticityToken = $('input[name="authenticity_token"]').val()
+  const headSha = $('input[name="head_sha"]').val()
+
+  $.ajax({
+    type: 'POST',
+    url: `${window.location.pathname}/reviews`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    data: {
+      '_method': 'put',
+      'authenticity_token': authenticityToken,
+      'head_sha': headSha,
+      'pull_request_review[event]': 'approve',
+      'pull_request_review[body]': 'LGTM'
+    },
+    beforeSend () {
+      formButtons.attr('disabled', true)
+    }
+  })
+  .done(() => formButtons.attr('disabled', false))
+}
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+
+  if (msg.action == 'submit_lgtm') {
+    submitLGTM()
+  }
+});
 
 $('#js-repo-pjax-container').bind("DOMSubtreeModified", init)
 
